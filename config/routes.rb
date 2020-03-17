37221062b25311e3213c4320b5d83a53ce3e7eed
goto-ui <onion753@gmail.com>
2020-03-17 14:15:19 +0000

    update Userモデルのnameとemailのvalidation完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
+
   root 'home#index'
   get  '/mariage', to: 'home#mariage'
   get  '/about_chocolat', to: 'home#about_chocolat'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get signup_path
+    get users_new_url
     assert_response :success
-    assert_select "title",full_title("Signup")
   end
+
 end

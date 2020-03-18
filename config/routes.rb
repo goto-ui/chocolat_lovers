bb194d73d8a69c37b919d5170c58265a6f0155d9
goto-ui <onion753@gmail.com>
2020-03-18 06:13:54 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
-
   root 'home#index'
   get  '/mariage', to: 'home#mariage'
   get  '/about_chocolat', to: 'home#about_chocolat'
   get '/signup', to: 'users#new'
-  
+  post '/signup',  to: 'users#create'
+  resources :users
 end

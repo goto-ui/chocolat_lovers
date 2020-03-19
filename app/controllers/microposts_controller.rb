4398bd88e7814328a590b4830d3c8311d67df726
goto-ui <onion753@gmail.com>
2020-03-19 15:00:00 +0000

    Add user microposts

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,35 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+  def new
+    @micropost = current_user.microposts.build if logged_in?
+  end
+
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to @micropost.user
+    else
+      render 'microposts/new'
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || @micropost.user
+  end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+end
